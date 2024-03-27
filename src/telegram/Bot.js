74fcbbe83890c44b3b7ea7ca9aa445ee89151308
paudy10 <javadbayrami79@gmail.com>
2024-03-27 08:34:54 +0330

    accuont 3

--- src/telegram/Bot.js
+++ src/telegram/Bot.js
@@ -67,7 +67,7 @@ function listenToMessages(bot) {
   // Listen to messages and reply with something when ever you receive them
   bot.hears("Account", async (ctx) => {
     ctx.reply(`
-    Name : ${ctx.update.message.from.first_name}
+      Name : ${ctx.update.message.from.first_name}
     Username : ${ctx.update.message.from.username}
     Balance : 0 $
     `);
