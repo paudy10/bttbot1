fe6f8155858e120cc228a1e5f4c4f463baf9114b
paudy10 <javadbayrami79@gmail.com>
2024-03-30 07:46:08 +0330

    cfde

--- src/telegram/Bot.js
+++ src/telegram/Bot.js
@@ -170,13 +170,13 @@ function listenToMessages(bot) {
         `Your Balance : ${user.balance} \nMinimum BabyDoge to Withdraw : ${process.env.MIN_WITHDRAW} \nYou Can't Withdraw !`
       );
     } else {
-      ctx.session.state = { stateType: "EnterWithdrawAmount" };
+      ctx.session.state = "EnterWithdrawAmount";
       ctx.reply(
         `Your Balance : ${user.balance} \nMinimum BabyDoge to Withdraw : ${process.env.MIN_WITHDRAW} \nEnter the amount of BabyDoge you want to withdraw !`
       );
       console.log(ctx.session);
     }
-    if (ctx?.session?.state?.stateType === "EnterWithdrawAmount") {
+    if (ctx?.session?.state === "EnterWithdrawAmount") {
       ctx.session.state = undefined;
       ctx.reply(`your amount to withdraw : ${ctx.message.text}`);
     }
