45aff85335931649dd060f7499c7a812aacc159c
paudy10 <javadbayrami79@gmail.com>
2024-03-30 04:57:14 +0330

    r

--- src/telegram/Bot.js
+++ src/telegram/Bot.js
@@ -193,7 +193,7 @@ function listenToQueries(bot) {
         //   { balance },
         //   { upsert: true }
         // );
-        console.log(ctx.message);
+        console.log(ctx.updates);
       }
       // ctx.reply(callback_data);
       // console.log(callback_data);
