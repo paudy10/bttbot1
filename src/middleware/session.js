49b69a49b57b8cdda5f0905b7154a14eac072268
paudy10 <javadbayrami79@gmail.com>
2024-03-31 03:45:40 +0330

    cefc

--- src/middleware/session.js
+++ src/middleware/session.js
@@ -6,32 +6,24 @@ export default function Session(ctx, next) {
   const state = ctx.session.state;
 
   if (state === "EnterWithdrawAmount") {
-    if (!ctx.message.text) {
-      ctx.session.state = undefined;
-    } else {
-      ctx.session.state = "EnterWallet";
-      ctx.session.amount = ctx.message.text;
-      ctx.reply(
-        `your amount to withdraw : ${ctx.message.text} \nSend Your Wallet Address !`
-      );
-    }
+    ctx.session.state = "EnterWallet";
+    ctx.session.amount = ctx.message.text;
+    ctx.reply(
+      `your amount to withdraw : ${ctx.session.amount} \nSend Your Wallet Address !`
+    );
   }
   if (state === "EnterWallet") {
-    if (!ctx.message.text) {
-      ctx.session.state = undefined;
-    } else {
-      ctx.session.wallet = ctx.message.text;
-      ctx.reply(
-        `your amount to withdraw : ${ctx.message.amount} \nyour wallet address : ${ctx.message.wallet} \nPress Confirm To withraw !`,
-        ConfirmWithdraw()
-      );
-    }
+    ctx.session.wallet = ctx.message.text;
+    ctx.reply(
+      `your amount to withdraw : ${ctx.session.amount} \nyour wallet address : ${ctx.session.wallet} \nPress Confirm To withraw !`,
+      ConfirmWithdraw()
+    );
   }
   if (state === "ConfirmWithdraw") {
     ctx.session.state = undefined;
     ctx.telegram.sendMessage(
       process.env.GP_ID,
-      `New Withdraw ! \n Amount to withdraw : ${ctx.message.amount} \nWallet address : ${ctx.message.wallet}`
+      `New Withdraw ! \n Amount to withdraw : ${ctx.session.amount} \nWallet address : ${ctx.session.wallet}`
     );
     ctx.reply(`withdraw successfull !`);
   }
