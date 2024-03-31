3fc22ab9dea21b3f5bc974436b1e595cda3d689e
paudy10 <javadbayrami79@gmail.com>
2024-03-31 03:53:38 +0330

    iep1

--- src/middleware/session.js
+++ src/middleware/session.js
@@ -13,6 +13,7 @@ export default function Session(ctx, next) {
     );
   }
   if (state === "EnterWallet") {
+    ctx.session.state = undefined;
     ctx.session.wallet = ctx.message.text;
     ctx.reply(
       `your amount to withdraw : ${ctx.session.amount} \nyour wallet address : ${ctx.session.wallet} \nPress Confirm To withraw !`,
