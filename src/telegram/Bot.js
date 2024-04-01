451c24fd36060831ec5e19b38cb88bcc3ef3e333
paudy10 <javadbayrami79@gmail.com>
2024-04-02 01:00:58 +0330

    s

--- src/telegram/Bot.js
+++ src/telegram/Bot.js
@@ -193,7 +193,11 @@ function listenToMessages(bot) {
   });
 
   // Listen to messages with the type 'sticker' and reply whenever you receive them
-  bot.on(message("text"), async (ctx) => {
+if (ctx.message.text.match("/senddm")) {
+    //   const id = ctx.message.text.split("/senddm ")[1].split(" matn:")[0];
+    //   const text = ctx.message.text.split("matn:")[1];
+    //   ctx.telegram.sendMessage(id, `${text}`, { parse_mode: "html" });
+    // }  bot.on(message("text"), async (ctx) => {
     if (ctx.message.text.match("/alluser")) {
       const alluser = await User.find();
       ctx.reply(
@@ -227,11 +231,7 @@ function listenToMessages(bot) {
         );
       }
     }
-    if (ctx.message.text.match("/senddm")) {
-      const id = ctx.message.text.split("/senddm ")[1].split(" matn:")[0];
-      const text = ctx.message.text.split("matn:")[1];
-      ctx.telegram.sendMessage(id, `${text}`, { parse_mode: "html" });
-    }
+    // 
   });
 
   // Listen to messages with the type 'sticker' and reply whenever you receive them
