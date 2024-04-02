f528611fed807bd4f89c984abaded03f409982ee
paudy10 <javadbayrami79@gmail.com>
2024-04-02 07:11:36 +0330

    ce

--- src/telegram/Bot.js
+++ src/telegram/Bot.js
@@ -124,7 +124,6 @@ function listenToCommands(bot) {
       );
     }
 
-    console.log(ctx.update.message);
     next();
   });
 
@@ -194,7 +193,8 @@ function listenToMessages(bot) {
 
   // Listen to messages with the type 'sticker' and reply whenever you receive them
   bot.on(message("text"), async (ctx) => {
-    console.log(ctx.message);
+    console.log(ctx.message.chat.id);
+    console.log(process.env.GP_ID);
     if (ctx.message.chat.id === process.env.GP_ID) {
       if (ctx.message.text.match("/alluser")) {
         const alluser = await User.find();
